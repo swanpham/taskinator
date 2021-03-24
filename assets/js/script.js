576e9dd9fa13486548a48033179c805e4a62301c
Swan Pham <accounting@nailsrus.ca>
2021-03-23 20:57:49 -0400

    index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function(event) {
+  event.preventDefault();
+  
+  
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
-buttonEl.addEventListener("click", createTaskHandler);
-
-// buttonEl.addEventListener("click", function() {
-//   var listItemEl = document.createElement("li");
-//   listItemEl.className = "task-item";
-//   listItemEl.textContent = "This is a new task.";
-//   tasksToDoEl.appendChild(listItemEl);
-// });
-
-// document.createElement("li");
-
-
-// // var taskItemEl = document.createElement("li");
-// // taskItemEl.textContent = "hello";
-// tasksToDoEl.appendChild(taskItemEl);
-// taskItemEl.className = "task-item";
-
+formEl.addEventListener("submit", createTaskHandler);
